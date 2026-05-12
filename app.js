bf2d861e5ad8501bb8ee321533b0098624747491
proyectosjdop-alfa <proyectos.jdop@gmail.com>
2026-05-12 07:39:37 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+function appEnProceso() {
+    // Una alerta personalizada sencilla
+    alert("APP en proceso: Estamos trabajando para integrar nuevas herramientas muy pronto.");
+}
+
+// Puedes añadir efectos de carga aquí si lo deseas
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Panel de Aplicaciones ENEE cargado correctamente.");
+});
